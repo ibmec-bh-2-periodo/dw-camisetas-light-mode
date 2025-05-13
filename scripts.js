8f3a0e00bad2429e8757b95152d5d23301f6afc5
Tamara Simões <tamara.backups@gmail.com>
2025-05-13 09:20:42 -0300

    Exemplos parciais

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 const lightMode = document.getElementsByTagName("img")[1];
 console.log(lightMode);
 
+const logoLight = document.createElement("img");
+logoLight.innerHTML = `<img src="./assets/img/Logo.png" alt="Logomarca da loja" />`;
+const paiDasLogos = document.getElementById("header");
+
+
 lightMode.addEventListener("click", function() {
     document.body.classList.toggle("modo-claro");
+    paiDasLogos.appendChild(logoLight);
 })
