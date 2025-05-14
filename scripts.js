1d405b86bba63bdac70a6621855fd93d2b3a59e8
Joao Pedro Dantas Thal <jpdantasthal@gmail.com>
2025-05-14 11:31:46 -0300

    Concluido

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
-const lightMode = document.getElementsByTagName('img')[1];
-console.log(lightMode)
 
-const logoLight = document.createElement('img')
-logoLight.innerHTML = '<img src="./assets/img/Mark.png" alt="Logomarca da loja"/>'
 
-const paiDasLogos = document.
+const logoPrincipal = document.getElementById('logo-principal')
+const toggle = document.getElementById('toogle')
+const stars = document.getElementById('estrelas')
+const logoSecundario = document.getElementById('logo-secundaria')
 
-lightMode.addEventListener('click', function(){
+toggle.addEventListener('click', function(){
     document.body.classList.toggle('modo-claro')
+    
+    if (document.body.classList.contains('modo-claro')) {
+        logoPrincipal.src = 'assets/img/Mark.png'
+        toggle.src = 'assets/img/ToogleLight.png'
+        stars.src = 'assets/img/StarsLight.png'
+        logoSecundario.src = 'assets/img/LogoLight.png'
+
+
+    }
+    else{
+        logoPrincipal.src = 'assets/img/logo.png'
+        toogle.src = 'assets/img/Toogle.png'
+        stars.src = 'assets/img/Stars.png'
+        logoSecundario.src = 'assets/img/Logo-2.png'
+    }
 })
