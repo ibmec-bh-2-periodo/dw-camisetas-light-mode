50b9688a39fd47d1a754bc7fec8cc805e2d1c048
Thomas Alane <thomhero10@gmail.com>
2025-05-14 11:25:29 -0300

    finish

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
-const lightMode = document.getElementsByTagName("img")[1];
-console.log(lightMode);
 
-const logoLight = document.createElement("img");
-logoLight.innerHTML = `<img src="./assets/img/Logo.png" alt="Logomarca da loja" />`;
-const paiDasLogos = document.getElementById("header");
+const principal = document.getElementById('principal')
+const toggle = document.getElementById('toggle')
+const stars = document.getElementById('stars')
+const logofooter = document.getElementById('logofooter')
 
-
-lightMode.addEventListener("click", function() {
+toggle.addEventListener("click", function() {
     document.body.classList.toggle("modo-claro");
-    paiDasLogos.appendChild(logoLight);
+    principal.src = './assets/img/Logo-Light.png';
+    if (document.body.classList.contains('modo-claro')) {
+        principal.src = './assets/img/Logo-Light.png';
+        toggle.src = './assets/img/Toogle-Light.png'
+        stars.src = 'assets/img/Stars-light.png'
+        logofooter.src = 'assets/img/Logo-tailwind-light.png'
+
+
+    } else {
+        principal.src = './assets/img/Logo.png'
+        toggle.src = './assets/img/Toogle.png'
+        stars.src = 'assets/img/Stars.png'
+        logofooter.src = 'assets/img/Logo-2.png'
+    }
 })
