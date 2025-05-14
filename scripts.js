84af31eaaf4dab0e25946f747b02fd0a3c9604da
Henrique Costa <hvgcosta@gmail.com>
2025-05-14 11:32:17 -0300

    game-over

--- scripts.js
+++ scripts.js
@@ -1,12 +1,26 @@
 const lightMode = document.getElementsByTagName("img")[1];
 console.log(lightMode);
 
-const logoLight = document.createElement("img");
-logoLight.innerHTML = `<img src="./assets/img/Logo.png" alt="Logomarca da loja" />`;
-const paiDasLogos = document.getElementById("header");
+const logoPrincipal = document.getElementById("logo-principal");
+const toggle = document.getElementById("toggle")
+const stars = document.getElementById("stars")
+const logoFooter = document.getElementById("logo-footer")
 
-
-lightMode.addEventListener("click", function() {
+toggle.addEventListener("click", function() {
     document.body.classList.toggle("modo-claro");
-    paiDasLogos.appendChild(logoLight);
-})
+    if (document.body.classList.contains("modo-claro")) {
+        logoPrincipal.src = "assets/img/Logo-Light.png";
+        toggle.src = "assets/img/Toogle-Light.png"
+        stars.src = "assets/img/Stars-light.png"
+        logoFooter.src = "assets/img/Logo-tailwind-light.png"
+
+    }
+    else {
+        logoPrincipal.src = "assets/img/Logo.png"
+        toggle.src = "assets/img/Toogle.png"
+        stars.src = "assets/img/Stars.png"
+        logoFooter.src = "assets/img/Logo-2.png"
+
+
+    }
+});
